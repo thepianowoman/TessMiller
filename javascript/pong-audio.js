67f68d3fbc8998c2104a1e6c8b1adc4933eb80a1
thepianowoman <tnmillr@yahoo.com>
2024-12-18 22:02:46 -0600

    Update pong-audio.js
    Well I changed the sounds back...

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,13 +43,13 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("MetalBacking.mp3"); //load sound
+export var wallSound = new soundFile("silence.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("MetalBacking.mp3");
+export var paddleSound = new soundFile("silence.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("MetalBacking.mp3");
+export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("MetalBacking.mp3");
@@ -57,12 +57,12 @@ soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("MetalBacking.mp3");
+export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("MetalBacking.mp3");
+export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
