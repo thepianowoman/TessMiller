0b9e056f633fdb2d85eb9771b6c75c103c1cfc93
thepianowoman <tnmillr@yahoo.com>
2025-01-02 09:24:30 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("silence.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("silence.mp3");
+export var paddleSound = new soundFile("powerchord.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");
